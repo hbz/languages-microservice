5fff90c685620a6a94e46b3990c5ba6be90e9448
Philipp von Böselager <philipp.boeselager@web.de>
2021-02-22 08:58:10 +0100

    Move ISO-639 table to Iso639Service.groovy

--- grails-app/controllers/languages/microservice/ApiController.groovy
+++ grails-app/controllers/languages/microservice/ApiController.groovy
@@ -2,21 +2,21 @@ package languages.microservice
 
 import grails.core.GrailsApplication
 import grails.plugins.*
-import groovy.json.JsonSlurper
 
-class ApiController implements PluginManagerAware {
+class ApiController implements PluginManagerAware{
 
     GrailsApplication grailsApplication
     GrailsPluginManager pluginManager
+    Iso639Service iso639Service
 
-    def index() {
+
+    def index(){
         [grailsApplication: grailsApplication, pluginManager: pluginManager]
     }
 
-    def listIso639two() {
+
+    def listIso639two(){
         response.setContentType("application/json")
-        File isoFile = new File(getClass().getResource(
-            "${File.separator}ISO-639-2_utf-8_EN_FR_DE.json").toURI())
-        render new JsonSlurper().parse(isoFile)
+        render iso639Service.getIso639Table()
     }
 }
---
+++ grails-app/services/languages/microservice/Iso639Service.groovy
@@ -0,0 +1,15 @@
+package languages.microservice
+
+import grails.gorm.transactions.Transactional
+import groovy.json.JsonSlurper
+
+@Transactional
+class Iso639Service {
+
+    static iso639Table = new JsonSlurper().parse(new File(getClass().getResource(
+        "${File.separator}ISO-639-2_utf-8_EN_FR_DE.json").toURI()))
+
+    def getIso639Table(){
+        return iso639Table
+    }
+}
