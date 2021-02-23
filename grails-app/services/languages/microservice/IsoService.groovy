4bbaff51480124966cf412f679d63363621209a2
Philipp von Böselager <philipp.boeselager@web.de>
2021-02-23 13:39:35 +0100

    Add resource handling for application server in IsoService

--- grails-app/services/languages/microservice/IsoService.groovy
+++ grails-app/services/languages/microservice/IsoService.groovy
@@ -6,8 +6,23 @@ import groovy.json.JsonSlurper
 @Transactional
 class IsoService{
 
-    static iso639Table = new JsonSlurper().parse(new File(getClass().getResource(
-        "${File.separator}ISO-639-2_utf-8_EN_FR_DE.json").toURI()))
+    static iso639Table
+
+    static{
+        JsonSlurper jsonSlurper = new JsonSlurper()
+        File resourceFile
+        String iso639ConfigFile = "${File.separator}ISO-639-2_utf-8_EN_FR_DE.json"
+        try{
+            // works for deployments in application servers
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader()
+            resourceFile = new File(classLoader.getResource(iso639ConfigFile).toURI())
+        }
+        catch (NullPointerException npe){
+            // works for local project configuration
+            resourceFile = new File(getClass().getResource(iso639ConfigFile).toURI())
+        }
+        iso639Table = jsonSlurper.parse(resourceFile)
+    }
 
     def getIso639Table(){
         return iso639Table
